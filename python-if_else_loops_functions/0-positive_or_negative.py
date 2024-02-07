755eead63a6744b25dc8e9e459037a03287cc748
ahmed0hamri <7mid7imri.com>
2024-02-07 19:08:55 +0100

    script1

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
 import random
-number = random.randint(-10000, 10000)
-if number < 0:
-    lst_dgt = number % -10
+number = random.randint(-10, 10)
+if number == 0:
+    print(f"{number} is zero")
+elif number < 0:
+    print(f"{number} is negative")
 else:
-    lst_dgt = number % 10
-if lst_dgt == 0:
-    print(f'Last digit of {number} is {lst_dgt} and is 0')
-elif lst_dgt < 6:
-    print(f'Last digit of {number} is {lst_dgt} and is less than 6 and not 0')
-else:
-    print(f'Last digit of {number} is {lst_dgt} and is greater than 5')
+    print(f"{number} is positive")
+    
