1c33b227964a76feeb5c68d42517f9baa73d6006
ahmed0hamri <7mid7imri.com>
2024-02-09 01:30:00 +0100

    script

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for i in range(10):
-    for k in range(10):
-        if i != k:
-            print(str(i) + str(k))
+for z in range(10):
+    for b in range(10):
+        if z != b:
+            print("{}{}, ".format(z, b), end="")
