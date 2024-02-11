12d5f61a1ee4a942a523b79844dc602fcacf1e9b
ahmed0hamri <7mid7imri.com>
2024-02-11 22:12:20 +0100

    script1

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -2,8 +2,3 @@
 def element_at(my_list, idx):
 
     return my_list[idx] if 0 < idx < len(my_list) else None
-
-
-list_b = ("b", "o", "t", "o", "m", "g")
-idx = 0
-print("Element at index {:d} is {}".format(idx, element_at(list_b, idx)))
