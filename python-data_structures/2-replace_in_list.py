7aa51bb597cab583475d0b815b0bc441f9351f57
ahmed0hamri <7mid7imri.com>
2024-02-13 22:46:29 +0100

    script2

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if 0 <= idx < len(my_list) and my_list and element
+    if 0 <= idx < len(my_list) and my_list and element:
+        my_list[idx] = element
     return my_list
