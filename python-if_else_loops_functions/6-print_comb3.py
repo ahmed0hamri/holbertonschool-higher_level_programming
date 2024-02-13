b6c69d5c79dc641d0829185218f8f4d847bc27e5
ahmed0hamri <7mid7imri.com>
2024-02-13 21:54:49 +0100

    script6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-for z in range(10):
-    for b in range(10):
-        if z != b:
-            print("{}{}, ".format(z, b), end="")
-else:
-    print("{}{}".format(z, b))
+#!/usr/bin/python3
+for j in range(10):
+    for d in range(10):
+        if j != d and j * 10 + d < d * 10 + j:
+            if j != 8 or d != 9:
+                print("{}{}, ".format(j, d), end="")
+            else:
+                print("{}{}".format(j, d))
