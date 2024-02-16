3ce1833bd0b9a5e15ee02b47901a63bd2eeb12e4
ahmed0hamri <7mid7imri.com>
2024-02-16 04:40:16 +0100

    script1

---
+++ python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" the file for the Square class definition
+"""
+
+
+class Square:
+    """a class that creates a square
+
+    Keyword arguments:
+    argument -- none
+    Return: none
+    """
+
+    def __init__(self, size):
+        self._Square__size = size
+        pass
