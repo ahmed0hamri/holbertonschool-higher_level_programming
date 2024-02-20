056a6f82011ea2e13fee153ea609469b1f827327
ahmed0hamri <7mid7imri.com>
2024-02-20 17:41:54 +0100

    script6

---
+++ python-classes/5-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" the file for the Square class definition
+"""
+
+
+from typing import Self
+
+
+class Square:
+    """a class that creates a square
+
+    Keyword arguments:
+    argument -- none
+    Return: none
+    """
+
+    def __init__(self, size=0):
+        self._Square__size = size
+
+    def area(self):
+        return self._Square__size ** 2
+
+    @property
+    def size(self):
+        return self._Square__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise Exception("size must be an integer")
+        if value < 0:
+            raise Exception("size must be >= 0")
+        self._Square__size = value
+
+    def my_print(self):
+    if Self._Square__size == 0:
+        print()
+    else:
+        square_line = "#" * self._Square__size
+        for _ in range(self._Square__size):
+            print(square_line)
+
