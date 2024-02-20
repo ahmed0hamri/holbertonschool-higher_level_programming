b96919d0153f2f561da12d07c3b80c143b049680
ahmed0hamri <7mid7imri.com>
2024-02-20 16:08:59 +0100

    scrpit3

---
+++ python-classes/2-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" the file for the Square class definition
+"""
+
+
+class Square:
+    """a class that creates a square
+
+    Keyword arguments:
+    argument -- none
+    Return: none
+    """
+
+    def __init__(self, size):
+        self._Square__size = size
+        pass
+
+
+class Square:
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
