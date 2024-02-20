63e633811c84079492dc0c22c62ea3db44a29a77
ahmed0hamri <7mid7imri.com>
2024-02-20 16:09:44 +0100

    scrpit3

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -11,11 +11,6 @@ class Square:
     Return: none
     """
 
-    def __init__(self, size):
-        self._Square__size = size
-        pass
-
-
 class Square:
     def __init__(self, size=0):
         if not isinstance(size, int):
