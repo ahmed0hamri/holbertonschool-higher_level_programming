dd3f3448f7472ab8db5b8c14224d6da2eafe1c0f
ahmed0hamri <7mid7imri.com>
2024-03-01 06:05:42 +0100

    ah yes rectangle

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+    This class creates an empty rectangle
+"""
+
+
+class Rectangle:
+    """
+    rectangle
+    """
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+
+"""
+    Property setter for the weight attribute, validating that the input is an
+    integer and greater than or equal to 0. Raises a TypeError otherwise.
+    """
+
+
+@property
+def width(self):
+    return self.__width
+
+@width.setter
+def width(self, value):
+    if not isinstance(width, int):
+        raise TypeError("width must be an integer")
+    elif width < 0:
+        raise ValueError("width must be >= 0")
+
+
+"""
+    Property setter for the height attribute, validating that the input is an
+    integer and greater than or equal to 0. Raises a TypeError otherwise.
+    """
+
+
+@property
+def height(self):
+    return self.__height
+
+@height.setter
+def height(self, value):
+    if not isinstance(height, int):
+        raise TypeError("height must be an integer")
+    elif height < 0:
+        raise ValueError("height must be >= 0")
