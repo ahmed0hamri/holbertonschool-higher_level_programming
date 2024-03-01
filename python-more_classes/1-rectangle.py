c67bdf5e86e941191283696a8ede805ea767edae
ahmed0hamri <7mid7imri.com>
2024-03-01 06:25:04 +0100

    ah yes rectangle

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -23,30 +23,39 @@ class Rectangle:
         return self.__height
 
 
+    """
+    Property setter for the weight attribute, validating that the input is an
+    integer and greater than or equal to 0. Raises a TypeError otherwise.
+    """
+
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
 
-        """
-        Property setter for the weight attribute, validating that the input is an
-        integer and greater than or equal to 0. Raises a TypeError otherwise.
-        """
-
+    """
+    Property setter for the weight attribute, validating that the input is an
+    integer and greater than or equal to 0. Raises a TypeError otherwise.
+    """
 
     @property
     def width(self):
         return self.__width
 
 
+    """
+    Property setter for the weight attribute, validating that the input is an
+    integer and greater than or equal to 0. Raises a TypeError otherwise.
+    """
+
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         self.__width
