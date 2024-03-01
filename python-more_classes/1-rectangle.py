92a06e731f43a62f3acccdd4a19dd65e5fe05273
ahmed0hamri <7mid7imri.com>
2024-03-01 06:10:37 +0100

    ah yes rectangle

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -28,7 +28,7 @@ def width(self):
 def width(self, value):
     if not isinstance(value, int):
         raise TypeError("width must be an integer")
-    elif width < 0:
+    elif value < 0:
         raise ValueError("width must be >= 0")
 
 
@@ -46,5 +46,5 @@ def height(self):
 def height(self, value):
     if not isinstance(value, int):
         raise TypeError("height must be an integer")
-    elif height < 0:
+    elif value < 0:
         raise ValueError("height must be >= 0")
