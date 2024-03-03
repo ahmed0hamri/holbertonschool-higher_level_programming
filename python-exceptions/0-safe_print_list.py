b67bcd7210b1dc41fba38c57018152241783d150
ahmed0hamri <7mid7imri.com>
2024-03-03 22:02:41 +0100

    script 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-
-
 def safe_print_list(my_list=[], x=0):
-    printed_elements = min(x, len(my_list)) 
-    for item in my_list[:x]:  
-        print(item, end=" ")
-    print() 
-    return printed_elements
-
-
-my_list = [1, 2, 3, 4, 5]
-num_printed = safe_print_list(my_list, 3)
-print("Number of elements printed:", num_printed)
+    printed_count = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end=" ")
+            printed_count += 1
+    except IndexError:
+        pass  # If the index is out of range, just continue without printing
+    finally:
+        print()  # Print a new line after printing the elements
+    return printed_count
