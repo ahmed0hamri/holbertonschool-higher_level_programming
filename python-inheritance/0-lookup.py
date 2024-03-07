d115f99d08c9d92d139ff25ec78905b47ae5f850
ahmed0hamri <7mid7imri.com>
2024-03-07 22:57:12 +0100

    script 0

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""
+    Inheritance: module file
+"""
 
 
 def lookup(obj):
+    """
+    lookup: a function that returns a list
+    containing available attributes and methods
+    of an object {obj}
+    """
     return dir(obj)
